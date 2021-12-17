db579d8d2d2e7bc317e15d2736f61a97c625d079
Akshay Dagar <akshaydagar98@gmail.com>
2021-12-17 19:11:06 +0530

    Creating GetPosts and CreatePost logics

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,23 @@
-export const GetPosts = (req,res) => {
-    res.send("<h1>In posts/ route</h1>");
+import PostMessage from "../models/PostMessage.js"
+
+export const GetPosts = async (req,res) => {
+    try {
+        const Posts = await PostMessage.find();
+        res.status(200).json(Posts);
+    } 
+    catch (error) {
+        res.status(404).json(error);
+    }
 }
 
-export const CreatePost = (req,res) => {
-    res.send("<h1>Post created</h1>")
+export const CreatePost = async (req,res) => {
+    try {
+        const NewPost = new PostMessage(req.body);
+        await NewPost.save();
+
+        res.status(201).json(NewPost);
+    } 
+    catch (error) {
+        res.status(409).json(error);    
+    }
 }

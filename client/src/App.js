fd67d8b794b32c71725088d53a6eeb845337da3f
Akshay Dagar <akshaydagar98@gmail.com>
2021-12-18 10:43:42 +0530

    Creating frontend structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
+import Memoirr from "./images/Memoirr.jpg";
+import Posts from "./Posts/Posts.js";
+import Form from "./Form/Form.js";
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Laning page</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memoirr</Typography>
+                <img src={Memoirr} alt="Memoirr" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignContent="stretch" spacing={3}>
+                        <Grid xl={12} xm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid xl={12} xm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
